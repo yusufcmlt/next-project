950517a38f8ff344a245700a4fefea01f2772697
yusuftokmak <yusuf.tokmak@grubtech.com>
2023-12-31 14:48:04 +0300

    added meta and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [searchText, setSearchText] = useState("");
   return (
     <>
+      <Head>
+        <title>Mobil Shop</title>
+        <meta name="description" content="E ticaret denemesi" />
+      </Head>
       <Navbar
         onSearch={(searchVal) => {
           setSearchText(searchVal);
